54c3627b72258dfaf1c0584327e4ca4f337afcd1
MgenGlder <mgen.glder@gmail.com>
2024-06-01 12:06:13 -0400

    Add dcc logo

--- server/server/settings.py
+++ server/server/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
