a30fa45f981a3332f55fde4c1fcbcf752aa684b8
MgenGlder <mgen.glder@gmail.com>
2024-09-14 13:44:03 -0400

    feat(api): Add scaffolded json response.

--- project/views.py
+++ project/views.py
@@ -1,1 +1,9 @@
 
+from django.http import JsonResponse
+
+def meetup_view(request):
+    data = {
+        "message": "Welcome to the meetup page!",
+        "status": "success"
+    }
+    return JsonResponse(data)
