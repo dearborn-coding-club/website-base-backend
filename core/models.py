aef413150fe97c78355a2b9ed5c2a56f6147e56c
MgenGlder <mgen.glder@gmail.com>
2024-09-25 16:10:18 -0400

    feat: Add first test; Start linting

--- core/models.py
+++ core/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from rest_framework import serializers
 
+
 class Note (models.Model):
-  title = models.CharField(max_length=100)
-  content = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True) 
-  updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_content(self):
+        return self.content
+
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from core.models import Note
 
-# Create your tests here.
+
+class NoteSerializerTest(TestCase):
+    def test_note_serializer_works(self):
+        """Animals that can speak are correctly identified"""
+        test_note = Note(content="test_content")
+        self.assertEqual(test_note.get_content(), 'test_content')
