9c0e06cfb717e1e116d70884c9a88aee2dd0e10b
MgenGlder <mgen.glder@gmail.com>
2024-10-13 11:45:31 -0400

    fix: Remove secret key from `settings.py` source code

--- public_api/settings.py
+++ public_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*1@f05-)o5!3+^kzs@r#y+$atj=!0q#rv%5wr34%3*3qw_d60'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
