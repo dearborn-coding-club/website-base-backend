66a79041a9532198daf3a616876b9ee7fa2381bd
MgenGlder <mgen.glder@gmail.com>
2024-12-07 20:43:56 -0500

    feat(api): Introduce AI writer

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import NoteViewSet
+from .views import NoteSummary
 
 router = DefaultRouter()
 router.register(r'v2/notes', NoteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('v2/notes/summary', NoteSummary),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,46 @@
 from rest_framework import viewsets
 from .models import Note
 from .serializers import NoteSerializer
+from django.http import JsonResponse
+from openai import OpenAI
+from django.db import connection
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+
+def NoteSummary(request):
+    client = OpenAI()
+    client.embeddings.create(
+        model="text-embedding-ada-002",
+        input="The food was delicious and the waiter...",
+        encoding_format="float"
+    )
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "write a haiku about ai"}
+        ]
+    )
+    rows = None
+    today = date.today()
+    last_month = today - relativedelta(months=1)
+    with connection.cursor() as cursor:
+        cursor.execute(f"""SELECT * FROM core_note where created_at > '{str(last_month)}' """)
+        rows = cursor.fetchall()
+    print(rows)
+    items = ""
+    for _, value in enumerate(rows):
+        items += value[1] + " "
+    embeddings = client.embeddings.create(
+        model="text-embedding-ada-002",
+        input="items",
+        encoding_format="float"
+    )
+
+    # return JsonResponse({"test": completion.choices[0].message.content})
+    return JsonResponse({"test": embeddings.data.__str__()})
