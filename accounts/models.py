7942bf489aa0cda7c468305acbc3390a3e942525
MgenGlder <mgen.glder@gmail.com>
2025-02-01 11:37:06 -0500

    feat: Add new migrations for updated user profile information.

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
-
 class Profile (models.Model):
     role = models.TextField()
     phone_number = models.TextField()
@@ -12,24 +11,49 @@ class Profile (models.Model):
     leetcode_username = models.TextField()
 
 
+class DCCPermission(Permission):
+    description = models.TextField(blank=True)
+
+class DCCGroup(Group):
+    # name = models.CharField(max_length=150, unique=True)
+    # permissions = models.ManyToManyField(
+    #     DCCPermission,
+    #     verbose_name='permissions',
+    #     blank=True,
+    # )
+    description = models.TextField(blank=True)
+    group_permissions = models.ManyToManyField(
+        DCCPermission,
+        related_name='dccgroup_set',  # Custom related name
+        blank=True,
+        help_text='The permissions this group has.',
+        verbose_name='permissions',
+    )
+
+    class Meta:
+        verbose_name = 'group'
+        verbose_name_plural = 'groups'
+
+    def __str__(self):
+        return self.name
+
+
 class DCCUser(AbstractUser):
     # Add any additional fields you need here
     bio = models.TextField(blank=True)
     birthdate = models.DateField(null=True, blank=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     groups = models.ManyToManyField(
-        Group,
-        related_name='customuser_set',  # Custom related name
+        DCCGroup,
+        related_name='dccuser_set',  # Custom related name
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='customuser_set',  # Custom related name
+        DCCPermission,
+        related_name='dccuser_set',  # Custom related name
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
     )
-
-
